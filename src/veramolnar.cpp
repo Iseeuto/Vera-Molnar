5c2429ffe40fb09809e5e171752ddd2d379417e3
Iseeuto <reporttanguy@gmail.com>
2024-04-19 14:54:15 +0200

    Final final

--- src/veramolnar.cpp
+++ src/veramolnar.cpp
@@ -21,5 +21,45 @@ int main(){
 
     svg_to_html("tab1.html", canvas_to_svg_veramolnar(C1, LTC1));
 
+    // Deuxième tableau
+
+    C2.width = 1000; C2.height = 1000;
+    C2.Rep_col = 10; C2.Rep_lig = 10;
+
+    C2.FC = file_to_composedObject("tab2.txt");
+    listTransformComposed LTC2 = file_to_transform("transfo2.txt");
+
+    svg_to_html("tab2.html", canvas_to_svg_veramolnar(C2, LTC2));
+
+    // Troisième tableau
+
+    C3.width = 1000; C3.height = 1000;
+    C3.Rep_col = 10; C3.Rep_lig = 10;
+
+    C3.FC = file_to_composedObject("tab3.txt");
+    listTransformComposed LTC3 = file_to_transform("transfo3.txt");
+
+    svg_to_html("tab3.html", canvas_to_svg_veramolnar(C3, LTC3));
+
+    // Quatrième tableau
+
+    C4.width = 1000; C4.height = 1000;
+    C4.Rep_col = 10; C4.Rep_lig = 10;
+
+    C4.FC = file_to_composedObject("tab4.txt");
+    listTransformComposed LTC4 = file_to_transform("transfo4.txt");
+
+    svg_to_html("tab4.html", canvas_to_svg_veramolnar(C4, LTC4));
+
+    // Cinquième tableau
+
+    C5.width = 1000; C5.height = 1000;
+    C5.Rep_col = 10; C5.Rep_lig = 10;
+
+    C5.FC = file_to_composedObject("tab5.txt");
+    listTransformComposed LTC5 = file_to_transform("transfo5.txt");
+
+    svg_to_html("tab5.html", canvas_to_svg_veramolnar(C5, LTC5));
+
     return 1;
 }
